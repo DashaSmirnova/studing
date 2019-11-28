df854db2836d8aa24bf281f5f60101b8a50ef415
Dasha <darya_smirnova_irk@mail.ru>
2019-11-28 15:20:18 +0800

    lab 1 td v0.3 added window input

--- lab2_testing.py
+++ lab2_testing.py
@@ -58,6 +58,8 @@ class Example(tk.Frame):
             self.canvas.bind("<MouseWheel>",self.zoomer)
             # Hack to make zoom work on Windows
             root.bind_all("<MouseWheel>",self.zoomer)
+            #inputWindow()            
+
      
         #move
         def move_start(self, event):
@@ -89,8 +91,91 @@ class Example(tk.Frame):
         def zoomerM(self,event):
             self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
             self.canvas.configure(scrollregion = self.canvas.bbox("all"))
-     
+
+
+# Второе окно со вводом данных             
+'''
+def inputWindow():
+    def printNum(height):
+        #global height
+        print(height.get())
+        print(1)
+    height = tk.StringVar()
+    window = tk.Tk()
+    window.geometry('400x400')
+    window.title('Второе окно')
+    lab_height  = tk.Label( window, text="Введи высоту(b)")
+    lab_height.pack()
+    
+    entry = tk.Entry(window, textvariable=height)
+    entry.pack()
+    but = tk.Button(window, text='Нарисовать фигуру и рассчитать площадь', command=printNum(height))
+
+  #  but.bind('<Button->', printNum())
+    but.pack()
+
+    #val = StringVar()
+   # val.set(val_.get())
+    #entry = Entry(window, textvariable=val)
+    #entry.pack()
+ 
+    window.mainloop()
+    
+'''
+def newWindow():
+                    global root
+                    root = tk.Tk()
+                    Example(root).pack(fill="both", expand=True)
+                    #window.mainloop()
+                    #root.destroy()
+def inputWindow():
+                def display_full_name():
+                    tk.messagebox.showinfo("GUI Python", height.get() + " " + width.get())
+                    global root
+                    try:
+                        root.destroy()
+                    except:
+                        pass
+                    #window.mainloop()
+                    newWindow()
+
+                    #inputWindow()
+                    #root.mainloop()
+
+                window = tk.Tk()
+                height = tk.StringVar( window)
+                width = tk.StringVar( window)
+                r = tk.StringVar( window)
+                R = tk.StringVar( window)
+                
+                height_label = tk.Label(window,text="Введите ширину:")
+                width_label = tk.Label(window,text="Введите высоту:")
+                r_label = tk.Label(window,text="Введите радиус внутреннего круга:")
+                R_label = tk.Label(window,text="Введите радиус кругов: по краям")
+                 
+                height_label.grid(row=0, column=0, sticky="w")
+                width_label.grid(row=1, column=0, sticky="w")
+                r_label.grid(row=2, column=0, sticky="w")
+                R_label.grid(row=3, column=0, sticky="w")
+                
+                height_entry = tk.Entry(window,textvariable=height)
+                width_entry = tk.Entry(window,textvariable=width)
+                r_entry = tk.Entry(window,textvariable=r)
+                R_entry = tk.Entry(window,textvariable=R)
+                
+                height_entry.grid(row=0,column=1, padx=5, pady=5)
+                width_entry.grid(row=1,column=1, padx=5, pady=5)
+                r_entry.grid(row=2,column=1, padx=5, pady=5)
+                R_entry.grid(row=3,column=1, padx=5, pady=5)     
+                 
+                message_button = tk.Button(window,text="Click Me", command=display_full_name)
+                message_button.grid(row=5,column=1, padx=5, pady=5, sticky="e")
+                window.mainloop()
+           
+    
 if __name__ == "__main__":
         root = tk.Tk()
         Example(root).pack(fill="both", expand=True)
+        inputWindow()
         root.mainloop()
+        
